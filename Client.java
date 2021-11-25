03a0fc118bb6600671952d4f5631d8e880747eb6
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-11-24 18:25:12 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -12,6 +12,33 @@ class client {
 
         //socket connection
         Socket clientSocket = new Socket("127.0.0.1", 6789);
+        
+        //used to read from the server
+        BufferedReader inFromServer =
+                new BufferedReader(new
+                        InputStreamReader(clientSocket.getInputStream()));
+        //used to send to the server
+        DataOutputStream outToServer =
+                new DataOutputStream(clientSocket.getOutputStream());
+
+        //used to read stuff into variables from the user
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        // HANDSHAKE ///////////////////////////////////////////
+
+        //prompt user for a name, which is sent to the server
+        System.out.print("Hello client!\n" +
+                "Please enter your name (will be provided to server): ");
+        //Save name from user input
+        NAME = inFromUser.readLine();
+        
+        //tell server the clients name
+        outToServer.writeBytes(NAME + '\n');
+
+        //ensure it gets confirmed
+        answer = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + answer);
 
         clientSocket.close();
     }
