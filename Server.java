d0ca19e0f3df2e7e3a27c21ebb1a1c2ccfd4d8d4
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2022-01-06 17:36:13 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.net.*;
+import java.time.*;
 
+class server {
+    //main thread of the program
+    //serves to initialize everything we need
+    //also establishes connection with clients & logs it
+    //+ spawns threads to deal with client requests
+    public static void main(String[] argv) throws Exception {
+        
+    }
+}
+
+//Basically serves as a "struct"
+//holds needed information to process a given clients request
+class ticket {
+    Socket client;
+    String clientName;
+    String equation;
+    public ticket(Socket c, String cN, String eq) {
+        client = c;
+        clientName = cN;
+        equation = eq;
+    }
+}
